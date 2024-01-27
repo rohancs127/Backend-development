f8c07873df4e30f408462c467e274559611759af
Rohan <rohankumar5803@gmail.com>
2024-01-27 11:26:10 +0530

    rest operator and spread operator code added

--- javascript-basics/array.js
+++ javascript-basics/array.js
@@ -1,14 +1,27 @@
 const array = [127,178,146,142,47];
-console.log(array);
+// console.log(array);
 
-console.log(array.length);
+// console.log(array.length);
 
-console.log(array.at(-1));
+// console.log(array.at(-1));
 
 const array2 = ['rohan', 127, {age: 28, attending: true}, {display:function(){console.log('Hello World')}}];
 
-console.log(array2);
+// console.log(array2);
 
-;
+array2.college  = "Sahyadri"
 
-console.log(array);
+// console.log(array2);
+
+//----------------------------------------------------------------
+//Rest operator-ES6
+
+let returnReceived = restOperator(array2);
+function restOperator(...a){
+    console.log(a);
+}
+
+const array3 = [...array2, 'babith']; // copy elements of array2 along with new element
+const array4 = [array2, 'babith']; //creates seperate 2 elements
+// console.log(array3);
+// console.log(array4);

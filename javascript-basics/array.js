80c2e83fb6eef6c348357f6da7093a59823913fe
Rohan <rohankumar5803@gmail.com>
2024-01-27 11:12:16 +0530

    array basics file created

---
+++ javascript-basics/array.js
@@ -0,0 +1,14 @@
+const array = [127,178,146,142,47];
+console.log(array);
+
+console.log(array.length);
+
+console.log(array.at(-1));
+
+const array2 = ['rohan', 127, {age: 28, attending: true}, {display:function(){console.log('Hello World')}}];
+
+console.log(array2);
+
+;
+
+console.log(array);
